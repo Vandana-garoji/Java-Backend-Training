11a1422d3d36d59cf3d890f825e7ff56eccef74a
Vandana-garoji <71265938+Vandana-garoji@users.noreply.github.com>
2022-07-12 23:14:04 +0530

    Add files via upload

---
+++ Library/src/com/iiht/training/elibrary/exception/ISBNAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package com.iiht.training.elibrary.exception;
+
+public class ISBNAlreadyExistsException extends RuntimeException
+{
+	
+	public ISBNAlreadyExistsException(String s)
+		{
+		super(s);
+		}
+}
+		
+
+
+
+
---
+++ Library/src/com/iiht/training/elibrary/exception/ISBNDoesNotExistsException.java
@@ -0,0 +1,13 @@
+package com.iiht.training.elibrary.exception;
+import java.io.*;
+
+public class ISBNDoesNotExistsException extends RuntimeException
+{
+	public ISBNDoesNotExistsException(String s)
+	{
+	super(s);
+	}
+}
+	
+
+
